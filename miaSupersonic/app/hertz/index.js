c49344cc504c5a50fc5e0dbea8e61928bd3dfe03
bashendriks <bashendriks14@msn.com>
2015-05-20 09:22:05 +0200

    new view

---
+++ miaSupersonic/app/hertz/index.js
@@ -0,0 +1,4 @@
+angular.module('hertz', [
+  // Declare any module-specific AngularJS dependencies here
+  'common'
+]);
---
+++ miaSupersonic/app/hertzcd../index.js
@@ -0,0 +1,4 @@
+angular.module('hertzcd..', [
+  // Declare any module-specific AngularJS dependencies here
+  'common'
+]);
